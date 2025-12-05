41c1e24812282aeeaf20d19b051f8afaa1cce698
Gus <gustavo.p.carvalho06@Outlook.com>
2025-12-05 10:33:52 -0300

    Fix Map

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -3,8 +3,8 @@ import { View, StyleSheet } from 'react-native';
 import MapComponent from '../components/MapComponent';
 
 export default function HomeScreen() {
-  const apiUrl = 'https://your-api-url.com'; 
-  const mapboxToken = 'pk.eyJ1Ijoic2FjYWJhbWJhc3BpcyIsImEiOiJjbWdxaDM5NHIycGxzMm1vZTk4c3oyZjI0In0.9FObgQi-9a8oDwxuEH0SrA'; 
+  const apiUrl = "http://10.0.2.2:3001";
+  const mapboxToken = 'pk.eyJ1Ijoic2FjYWJhbWJhc3BpcyIsImEiOiJjbWdxaDM5NHIycGxzMm1vZTk4c3oyZjI0In0.9FObgQi-9a8oDwxuEH0SrA';
 
   return (
     <View style={styles.container}>
