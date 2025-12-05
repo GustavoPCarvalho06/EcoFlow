d85ca1383aa15b71971d1c5454d88c2b9d3bd9d4
MiguelAr <miguelarthurdasilvabarros@gmail.com>
2025-12-04 21:34:10 -0300

    tirei o log onde mostrava as informação sobre o usuario

--- front/src/middleware.js
+++ front/src/middleware.js
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import jwt from 'jsonwebtoken';
 
-// Defina os prefixos das rotas que você quer proteger.
 const protectedPrefixes = ["/dashboard", "/comunicados", "/perfil", "/mensagens", "/mapa", "/usuarios"];
 
 export function middleware(request) {
   const { pathname } = request.nextUrl;
 
-  // Obtém o token e o token (cargo do usuário) dos cookies.
   const cookieStore = cookies();
   const tokenCookie = cookieStore.get('token');
 
@@ -23,9 +21,7 @@ export function middleware(request) {
   }
 
 
-  console.log("Acessando a rota:", pathname);
-  console.log("token do usuário (cookie):", user);
-  console.log("Decoded token cargo:", user?.cargo);
+
 
 
   const isProtected = protectedPrefixes.some((prefix) =>
